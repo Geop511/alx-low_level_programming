de8d13213ec0a64d4aff2e491b486c65c707d948
Geop511 <oderinde.peter01@gmail.com>
2022-12-13 11:13:52 -0800

    completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry Block
+ * Description : print _putchar followed by a nw line
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
